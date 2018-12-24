b7eacecbbfe65a3aff02122ae499e27165644ed3
Ngan Nguyen <better.ngan@1pac.vn>
2018-12-24 14:20:15 +0700

    completetodo

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,73 @@
 window.addEventListener('DOMContentLoaded', function () {
   const input = document.querySelector('#new-input');
   const list = document.querySelector('#todo-list');
+  let itemList = document.getElementsByTagName('li');
   const bottom = document.querySelector('.bottom-wrap');
   const countDisplay = document.querySelector('#item-count');
 
   let deleteBtns = document.getElementsByClassName('delete-btn');
-  let listItems = document.getElementsByTagName('li');
-  let count = 0;
+  let completeBtns = document.getElementsByClassName('complete-btn');
 
+  let count = 0;
+  let completedItems = document.getElementsByClassName('hide');
+
+  // ADD TODOS
   input.addEventListener('keyup', function (e) {
     if (e.keyCode == 13) {
-      const listItem = document.createElement('li');
+      let item = document.createElement('li');
       if (input.value !== '') {
-        listItem.innerHTML = `${input.value}<span class="delete-btn">\u00D7</span>`;
-        list.appendChild(listItem);
-        count += 1;
+        item.innerHTML = `<span class="complete-btn"><span class="complete-icon hide">&#10003;</span></span> <span>${input.value}</span><span class="delete-btn">\u00D7</span>`;
+        list.appendChild(item);
+
         input.value = '';
+        count += 1;
         countDisplay.innerHTML = count;
 
+        bottom.style.display = 'flex';
+
+        // DELETE TODOS
         for (let i = 0; i < deleteBtns.length; i++) {
           deleteBtns[i].onclick = function() {
-            var li = this.parentElement;
-            li.style.display = 'none';
-            count -= 1;
-            countDisplay.innerHTML = count;
+            let li = this.parentElement;
+            li.parentElement.removeChild(li);
 
-            if (count < 1) {
+            if (count > 0) {
+              count -= 1;
+              countDisplay.innerHTML = count;
+            } else {
+              countDisplay.innerHTML = 0;
+            }
+
+            if (itemList.length < 1) {
               bottom.style.display = 'none';
             }
           }
         }
 
-        if (listItems.length > 0) {
-          bottom.style.display = 'flex';
+        // COMPLETE TODOS
+        for (let i = 0; i < completeBtns.length; i++) {
+          completeBtns[i].onclick = function () {
+            let li = this.parentElement;
+            let liText = li.getElementsByTagName('span')[2];
+            let completeIcon = this.firstChild;
+
+            if (completeIcon.classList.contains('hide') == true) {
+              liText.style.textDecoration = 'line-through';
+              completeIcon.classList.remove('hide');
+
+              if (count < 0) {
+                countDisplay.innerHTML = 0;
+              } else {
+                count -= 1;
+                countDisplay.innerHTML = count;
+              }
+            } else {
+              liText.style.textDecoration = 'none';
+              completeIcon.classList.add('hide');
+              count += 1;
+              countDisplay.innerHTML = count;
+            }
+          }
         }
       }
     }
