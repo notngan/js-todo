8f776ddf92147c451099313b64f685ba0d3a3bc3
Ngan Nguyen <better.ngan@1pac.vn>
2018-12-24 12:01:57 +0700

    count items

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,40 @@
 window.addEventListener('DOMContentLoaded', function () {
-  let input = document.querySelector('#new-input');
-  let list = document.querySelector('#todo-list');
+  const input = document.querySelector('#new-input');
+  const list = document.querySelector('#todo-list');
+  const bottom = document.querySelector('.bottom-wrap');
+  const countDisplay = document.querySelector('#item-count');
 
-  const deleteBtns = document.getElementsByClassName('delete-btn');
+  let deleteBtns = document.getElementsByClassName('delete-btn');
+  let listItems = document.getElementsByTagName('li');
+  let count = 0;
 
   input.addEventListener('keyup', function (e) {
     if (e.keyCode == 13) {
-      let listItem = document.createElement('li');
+      const listItem = document.createElement('li');
       if (input.value !== '') {
-        const inputText = document.createTextNode(input.value);
-        listItem.appendChild(inputText);
+        listItem.innerHTML = `${input.value}<span class="delete-btn">\u00D7</span>`;
         list.appendChild(listItem);
+        count += 1;
         input.value = '';
-
-        const span = document.createElement('span');
-        const spanText = document.createTextNode("\u00D7");
-        span.appendChild(spanText);
-        span.classList.add('delete-btn');
-        listItem.appendChild(span);
+        countDisplay.innerHTML = count;
 
         for (let i = 0; i < deleteBtns.length; i++) {
           deleteBtns[i].onclick = function() {
             var li = this.parentElement;
-            li.style.display = "none";
+            li.style.display = 'none';
+            count -= 1;
+            countDisplay.innerHTML = count;
+
+            if (count < 1) {
+              bottom.style.display = 'none';
+            }
           }
+        }
+
+        if (listItems.length > 0) {
+          bottom.style.display = 'flex';
         }
       }
     }
   })
-
-  function deleteItem() {
-    console.log('delete');
-  }
 });
