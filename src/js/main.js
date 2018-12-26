be39f911e0432f416adaf806ef673bb0a6537990
Ngan Nguyen <better.ngan@1pac.vn>
2018-12-26 11:20:36 +0700

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
+import { fail } from "assert";
+
 const contentArr = [];
 const input = document.querySelector('.new-input');
 const btnActive = document.querySelector('.btn-active');
 const btnAll = document.querySelector('.btn-all');
 const btnComplete = document.querySelector('.btn-complete');
+const bottom = document.querySelector('.bottom-wrap');
 
 window.addEventListener('DOMContentLoaded', function () {
-
   displayStorage();
 
   input.addEventListener('keydown', addTodo);
@@ -18,29 +20,7 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 
 //FUNCTIONS
-function showActive() {
-  const activeItems = document.querySelectorAll('.hide');
-  const completeItems = document.querySelectorAll('.complete');
-  addClass(completeItems, 'display-none');
-  removeClass(activeItems, 'display-none');
-  addButtonClass(this);
-}
 
-function  showComplete() {
-  const completeItems = document.querySelectorAll('.complete');
-  const activeItems = document.querySelectorAll('.hide');
-  addClass(activeItems, 'display-none');
-  removeClass(completeItems, 'display-none');
-  addButtonClass(this);
-}
-
-function showAll() {
-  const itemList = document.querySelectorAll('li');
-    Array.from(itemList).forEach(item => {
-      item.classList.remove('display-none');
-    });
-  addButtonClass(this);
-}
 
 function addTodo(e) {
   //return if not key Enter
@@ -51,12 +31,17 @@ function addTodo(e) {
 
   if (input.value !== '') {
     li.innerHTML =
-    `<span class="complete-btn"><span class="complete-icon hide">&#10003;</span></span>
+    `<span class="complete-btn"><span class="complete-icon">&#10003;</span></span>
     <span class="content" contentEditable="false">${input.value}</span>
     <span class="delete-btn">\u00D7</span>`;
 
+    if (btnComplete.classList.contains('active')) {
+      li.classList.add('display-none');
+    }
+
     ul.appendChild(li);
 
+    // save to storage
     contentArr.push({
       text: input.value,
       isCompleted: false
@@ -68,12 +53,10 @@ function addTodo(e) {
     // reset input
     input.value = '';
     showBottom();
-    countUncomplete();
+    countActive();
 
     li.querySelector('.content').addEventListener('dblclick', editTodo);
-
     li.querySelector('.complete-btn').addEventListener('click', completeTodo);
-
     li.querySelector('.delete-btn').addEventListener('click', deleteTodo);
   }
 }
@@ -88,15 +71,13 @@ function displayStorage() {
   for (let i = 0; i < store.length; i++) {
     let li = document.createElement('li');
 
-    li.innerHTML = `<span class="complete-btn"><span class="complete-icon hide">&#10003;</span></span>
+    li.innerHTML = `<span class="complete-btn"><span class="complete-icon">&#10003;</span></span>
     <span class="content" contentEditable="false">${store[i].text}</span>
     <span class="delete-btn">\u00D7</span>`
 
     // add completed style
     if (store[i].isCompleted) {
-      li.querySelector('.complete-icon').classList.remove('hide');
-      li.querySelector('.complete-icon').classList.add('complete');
-      li.querySelector('.content').classList.add('complete-text');
+      li.classList.add('complete');
     }
 
     li.querySelector('.complete-btn').addEventListener('click', completeTodo);
@@ -106,14 +87,14 @@ function displayStorage() {
     ul.appendChild(li);
 
     showBottom()
-    countUncomplete();
+    countActive();
   }
 }
 
 function deleteTodo() {
   const todoItems = document.querySelectorAll('li');
   const li = this.parentElement;
-  li.parentElement.removeChild(li);
+  const ul = this.parentElement.parentElement
 
   for (let key in contentArr) {
     contentArr.splice(key, 1);
@@ -122,7 +103,9 @@ function deleteTodo() {
     break;
   }
 
-  countUncomplete();
+  ul.removeChild(li);
+
+  countActive();
 
   if (todoItems.length < 2) {
     hideBottom();
@@ -149,43 +132,16 @@ function editTodo() {
 
 function completeTodo() {
   const li = this.parentElement;
-  const liText = li.querySelector('.content');
-  const completeIcon = this.querySelector('.complete-icon');
   const index = Array.prototype.indexOf.call(li.parentElement.childNodes, li)
 
-  // item is active
-  if (completeIcon.classList.contains('hide')) {
-    liText.classList.add('complete-text');
-    completeIcon.classList.remove('hide');
-    completeIcon.classList.add('complete');
-
-    // hide completed item in active tab
-    if (btnActive.classList.contains('active')) {
-      li.classList.add('display-none');
-    }
-
-    countUncomplete();
-
-    for (let key in contentArr) {
-      if (index == key) {
-        contentArr[key].isCompleted = true;
-        localStorage.clear();
-        localStorage.setItem('list', JSON.stringify(contentArr));
-        break;
-      }
-    }
-    
-  } else { // item is completed
-    liText.classList.remove('complete-text');
-    completeIcon.classList.remove('complete');
-    completeIcon.classList.add('hide');
-
-    // hide active item in complete tab
+  // item is completed
+  if (li.classList.contains('complete')) {
+    li.classList.remove('complete');
     if (btnComplete.classList.contains('active')) {
       li.classList.add('display-none');
     }
 
-    countUncomplete();
+    countActive();
 
     for (let key in contentArr) {
       if (index == key) {
@@ -195,22 +151,69 @@ function completeTodo() {
         break;
       }
     }
+
+  } else { // item is active
+    li.classList.add('complete');
+    if (btnActive.classList.contains('active')) {
+      li.classList.add('display-none');
+    }
+
+    countActive();
+
+    for (let key in contentArr) {
+      if (index == key) {
+        contentArr[key].isCompleted = true;
+        localStorage.clear();
+        localStorage.setItem('list', JSON.stringify(contentArr));
+        break;
+      }
+    }
   }
 }
 
-function addClass(arr, className) {
-  Array.from(arr).forEach(item => {
-    item.parentElement.parentElement.classList.add(className);
-  });
+function countActive() {
+  const actives = document.querySelectorAll('li:not(.complete');
+  const countDisplay = document.querySelector('.item-count');
+  countDisplay.innerHTML = actives.length;
 }
 
-function removeClass(arr, className) {
-  Array.from(arr).forEach(item => {
-    item.parentElement.parentElement.classList.remove(className);
+function showActive() {
+  const activeItems = document.querySelectorAll('li:not(.complete)');
+  const completeItems = document.querySelectorAll('.complete');
+
+  Array.from(activeItems).forEach(item => {
+    item.classList.remove('display-none');
   });
+
+  Array.from(completeItems).forEach(item => {
+    item.classList.add('display-none');
+  });
+  addButtonStyle(this);
 }
 
-function addButtonClass(element) {
+function showComplete() {
+  const activeItems = document.querySelectorAll('li:not(.complete)');
+  const completeItems = document.querySelectorAll('.complete');
+
+  Array.from(completeItems).forEach(item => {
+    item.classList.remove('display-none');
+  });
+
+  Array.from(activeItems).forEach(item => {
+    item.classList.add('display-none');
+  });
+  addButtonStyle(this);
+}
+
+function showAll() {
+  const allItems = document.querySelectorAll('li');
+  Array.from(allItems).forEach(item => {
+    item.classList.remove('display-none');
+  });
+  addButtonStyle(this);
+}
+
+function addButtonStyle(element) {
   const btns = document.querySelectorAll('.btn');
   Array.from(btns).forEach(item => {
     item.classList.remove('active');
@@ -218,18 +221,10 @@ function addButtonClass(element) {
   element.classList.add('active');
 }
 
-function countUncomplete() {
-  const countDisplay = document.querySelector('#item-count');
-  const completeClass = document.querySelectorAll('.hide');
-  countDisplay.innerHTML = completeClass.length;
-}
-
 function showBottom() {
-  const bottom = document.querySelector('.bottom-wrap');
   bottom.style.display = 'flex';
 }
 
 function hideBottom() {
-  const bottom = document.querySelector('.bottom-wrap');
   bottom.style.display = 'none';
 }
